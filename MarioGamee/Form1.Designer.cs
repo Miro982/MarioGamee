153b7c71b60583f3140ba699990fb0eede634be9
Miro982 <63558477+Miro982@users.noreply.github.com>
2020-06-02 21:24:50 +0300

    Add project files.

---
+++ MarioGamee/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace MarioGamee
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.mario = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.txtscore = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mario)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MarioGamee.Properties.Resources.mariobrick;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 400);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(801, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // mario
+            // 
+            this.mario.BackColor = System.Drawing.Color.Transparent;
+            this.mario.Image = global::MarioGamee.Properties.Resources.MarioRun;
+            this.mario.Location = new System.Drawing.Point(66, 342);
+            this.mario.Name = "mario";
+            this.mario.Size = new System.Drawing.Size(69, 74);
+            this.mario.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mario.TabIndex = 1;
+            this.mario.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::MarioGamee.Properties.Resources.spike1;
+            this.pictureBox3.Location = new System.Drawing.Point(282, 368);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(36, 32);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "obstacle";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = global::MarioGamee.Properties.Resources.spike_2;
+            this.pictureBox4.Location = new System.Drawing.Point(569, 368);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(43, 33);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 1;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "obstacle";
+            // 
+            // txtscore
+            // 
+            this.txtscore.AutoSize = true;
+            this.txtscore.BackColor = System.Drawing.Color.Transparent;
+            this.txtscore.Location = new System.Drawing.Point(12, 9);
+            this.txtscore.Name = "txtscore";
+            this.txtscore.Size = new System.Drawing.Size(47, 13);
+            this.txtscore.TabIndex = 2;
+            this.txtscore.Text = "Score: 0";
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 20;
+            this.Timer.Tick += new System.EventHandler(this.MainGameTimerEvent);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MarioGamee.Properties.Resources.MarioBackground;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtscore);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.mario);
+            this.Name = "Form";
+            this.Text = "Mario Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mario)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox mario;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label txtscore;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ MarioGamee/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarioGamee
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        bool jumping = false;
+        int jumpSpeed;
+        int force = 14;
+        int score = 0;
+        int obstacleSpeed = 11;
+
+        Random rand = new Random();
+        int position;
+        bool isGameOver = false;
+
+        public Form()
+        {
+            InitializeComponent();
+            GameReset();
+        }
+
+        private void MainGameTimerEvent(object sender, EventArgs e)
+        {
+            mario.Top += jumpSpeed;
+            txtscore.Text = "Score: " + score;
+            if (jumping==true && force < 0)
+            {
+                jumping = false;
+            }
+            if (jumping == true)
+            {
+                jumpSpeed = -14;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 14;
+            }
+            if(mario.Top > 342 && jumping == false)
+            {
+                force = 14;
+                mario.Top = 343;
+                jumpSpeed = 0;
+            }
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    x.Left -= obstacleSpeed;
+                    if (x.Left < -100)
+                    {
+                        x.Left = this.ClientSize.Width + rand.Next(200, 500) + (x.Width * 15);
+                        score++;
+                    }
+
+                    if (mario.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        Timer.Stop();
+                        mario.Image = Properties.Resources.dead;
+                        txtscore.Text += "Press N to play the game again:";
+                        isGameOver = true;
+
+                    }
+                }
+            }
+            if(score >3)
+            {
+                obstacleSpeed = 17;
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && jumping == false)
+            {
+                jumping = true;
+                 
+                    
+                }
+            
+            }
+        
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if(jumping == true)
+            {
+                jumping = false;
+            }
+
+            if(e.KeyCode == Keys.N && isGameOver == true)
+            {
+                GameReset();
+            }
+        }
+        private void GameReset()
+            {
+            force = 14;
+            jumpSpeed = 0;
+            jumping = false;
+            score = 0;
+            obstacleSpeed = 11;
+            txtscore.Text = "Score: " + score;
+            mario.Image = Properties.Resources.MarioRun;
+            isGameOver = false;
+            mario.Top = 342;
+
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox &&(string)x.Tag=="obstacle")
+                {
+                    position = this.ClientSize.Width + rand.Next(500,800) + (x.Width + 10);
+                    x.Left = position;
+                }
+            }
+            Timer.Start();
+        }
+    }
+}
